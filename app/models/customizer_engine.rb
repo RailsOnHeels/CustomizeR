5772428f12b43831a9505b14b93267df59ed054f
Pallavi <pallavi.mathane@sv.cmu.edu>
2013-10-25 22:32:16 -0700

    Richer library

--- app/models/customizer_engine.rb
+++ app/models/customizer_engine.rb
@@ -9,7 +9,16 @@ Experience in both B2B and B2C online marketing"
 
 
 @keywords = {'web development' => ['web development', 'web design'], 'program management' => ['program management',
-                'program management']}
+                'project management'], 'UX' => ['user experience', 'UED', 'UX', 'user experience design', 'HCI',
+                'human computer interaction'], 'iOS apps' => ['iOS apps', 'mobile app development', 'objective c'],
+                'git' => ['git', 'github'], 'agile' => ['agile', 'scrum', 'co-location', 'pair programming','tdd',
+                'test driven development', 'refactor', 'code refactoring', 'continuous integration', 'XP',
+                'extreme programming', 'story', 'velocity'], 'rest' => ['rest', 'rest api', 'get post put delete',
+                'representational state transfer', 'web API'], 'json' => ['json', 'javascript object notation'],
+                'csm' => ['scrum master certified', 'csm', 'certification for scrum'], 'sdlc' => ['sdlc',
+                'software development life cycle', 'application development life cycle',
+                'software development life-cycle'], 'html' => ['html', 'css']
+              }
 
 
 @skills_keywords_in_job = nil
