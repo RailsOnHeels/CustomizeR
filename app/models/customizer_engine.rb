1c835e102a036c917520628ec9407c60cb2e5bd4
Maria Emilia Torino <emilia.torino@west.cmu.edu>
2013-10-25 20:26:50 -0700

    Adding sample code so Pal can add rich content

--- app/models/customizer_engine.rb
+++ app/models/customizer_engine.rb
@@ -1,3 +1,20 @@
 class CustomizerEngine
 
+@job_description = "Bachelors or a Master's degree with minimum of 6+ years' experience in web development and program management.
+Experience in both B2B and B2C online marketing"
+
+#tolower
+
+@people_skills = ['web development', 'program management', 'B2B', 'B2C', 'online marketing', 'marketing', 'java', ]
+
+
+@keywords = {'web development' => ['web development', 'web design'], 'program management' => ['program management',
+                'program management']}
+
+
+@skills_keywords_in_job = nil
+
+
+
+
 end
