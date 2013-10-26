a622736200da3fe5b9309408531dfd3dc1fd94b6
Maria Emilia Torino <emilia.torino@west.cmu.edu>
2013-10-26 03:07:45 -0700

    Interacting with model

--- app/controllers/authlinks_controller.rb
+++ app/controllers/authlinks_controller.rb
@@ -1,5 +1,6 @@
 class AuthlinksController < ApplicationController
 
+
     def link
       client = LinkedIn::Client.new('dgdg77vt2f48', 'ToYDzQsG95imJXk6')
       request_token = client.request_token(:oauth_callback => "http://#{request.host_with_port}/Authlinks/authenticate_link")
@@ -30,9 +31,20 @@ class AuthlinksController < ApplicationController
       @positions = client.profile(:fields => [:positions]).positions.all
       @saved_jobs = client.job_bookmarks(:fields => [:job, :is_applied, :is_saved])
 
+      #@updated_profile = reorganize_profile(@profile, @saved_jobs.all[0])
       render :action => :info
 
     end
 
+    def reorganize_profile(user_profile, saved_job)
+      @customizer_engine = CustomizerEngine.new
+      @customizer_engine.set_user_skills(user_profile.skills)
+      @customizer_engine.set_job_description(saved_job.job.description_snippet)
+      @job_skills = @customizer_engine.get_keywords_from_job_description
+      @customizer_engine.match_keywords_to_user_skills
+      @highlighted_skills = @customizer_engine.highlight_user_skills_based_on_job_skills(@job_skills)
+      @output = @customizer_engine.group_by_keyword
+
+    end
 
   end
